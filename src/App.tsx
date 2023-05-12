018e845600cdd45d930f1ceba3d58355b0d48da6
Milenko Castillo <milenko.castillo@gmail.com>
2023-05-12 13:36:23 -0400

    basci html elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <label id="break-label" htmlFor="">Break Length</label>
+      <label id="session-label" htmlFor="">Session Length</label>
+      <input id="break-increment" type="button" value="" />
+      <input id="break-decrement" type="button" value="" />
+      <input id="session-increment" type="button" value="" />
+      <input id="session-decrement" type="button" value="" />
+      <div id="break-length" >5</div>
+      <div id="session-length" >25</div>
+      <label id="timer-label" htmlFor="">Session</label>
+      <div id="time-left" ></div>
+      <input id="start_stop" type="button" />
+      <input id="reset" type="button" />
     </>
   )
 }
